24092b2c8e588f9c256d2ed8863cbef08eede2a6
circus2271 <rodionov134@gmail.com>
2023-05-14 04:47:39 +0300

    hm

---
+++ final-client-v2/README.md
@@ -0,0 +1,25 @@
+для локального запуска можно использовать что-то типа https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb
+
+
+добавить функцию insertingLikeDislikeFormData для списка:
+- [x] playCeylonNew
+- [x] playChampNew
+- [x] playGooseGoose-1floorNew
+- [x] playGooseGoose-2floorNew
+- [x] playHitch-MosNew
+- [x] playLamvilNew
+- [x] playLocalsNew
+- [x] playMINANew
+- [x] playRoninNew
+- [x] playSalutNew
+- [x] sowokNew
+- [x] playAnteNew
+- [x] playBruxxNew
+- [x] playCrepesNew
+- [x] playHitch-MedikovNew
+- [x] playHuanNew
+- [x] playPivzavodNew
+- [x] playProfsoyuzNew
+- [x] playPrognozNew
+- [x] playsaloneNew
+- [x] yulaNew
---
+++ server-airtable/sphere-google-cloud/src/index.js
@@ -0,0 +1,118 @@
+const axios = require('axios');
+require('dotenv').config()
+
+const {
+  API_KEY,
+  BASE_ID,
+  TABLE_ID,
+} = process.env
+
+const AIRTABLE_TABLE_API_ENDPOINT = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_ID}`;
+
+const headers = {
+  'Authorization': `Bearer ${API_KEY}`,
+};
+
+/**
+ *
+ * @param (string) filterString (e.g. 'Playing', 'Like', 'Disliked')
+ */
+const getFilteredRecords = async (filterString) => {
+  const allRecords = [];
+  let _offset;
+  
+  do {
+    const response = await axios.get(`${AIRTABLE_TABLE_API_ENDPOINT}`, {
+      headers,
+      params: {
+        offset: _offset ? _offset : '',
+        // how to filter data by key (in airtable)
+        // https://help.landbot.io/article/75ax4g3ogr-11-different-ways-to-get-and-filter-data-from-airtable#2_one_fixed_filter
+        filterByFormula: `SEARCH("${filterString}",{Status})`
+      }
+    });
+    
+    const { records, offset } = response.data;
+    // console.log('offset', offset)
+    allRecords.push(...records);
+    _offset = offset;
+  } while (_offset);
+  return allRecords;
+}
+
+/**
+ *
+ * @param (string) recordId
+ * @param (string) newStatusSingleValueString (e.g. 'Playing', 'Like', 'Dislike')
+ */
+const updateRecordStatus = async (recordId, newStatusSingleValueString) => {
+  const record = await getRecord(recordId);
+  const currentStatusArray = record.fields.Status || []; // because empty Status is undefined by default
+  
+  if (currentStatusArray.includes(newStatusSingleValueString)) {
+    return;
+  }
+  
+  const updatedStatusArray = [...currentStatusArray, newStatusSingleValueString];
+  const response = await patchRecord(recordId, updatedStatusArray);
+  
+  return response;
+}
+
+const getRecord = async (recordId) => {
+  const response = await axios.get(`${AIRTABLE_TABLE_API_ENDPOINT}/${recordId}`, {
+    headers,
+  })
+  
+  return response.data
+}
+
+const patchRecord = async (recordId, updatedStatusArray) => {
+  const response = await axios.patch(`${AIRTABLE_TABLE_API_ENDPOINT}/${recordId}`, {
+    fields: {
+      Status: updatedStatusArray
+    }
+  }, {
+    headers
+  })
+  
+  return response.data
+}
+
+
+// test function to run on each code change
+// if server is ready
+const autorun = async () => {
+  // return;
+  const RECORD_ID = 'rec7th1mbahsx1BdT'
+  
+  console.log('autorun function start')
+  try {
+    await updateRecordStatus(RECORD_ID, 'Playing')
+    // try {
+    //   const response = await getRecord(RECORD_ID);
+    //   console.log('getRecord() response data:', response)
+    // try {
+    //   const response = await patchRecord(RECORD_ID, ['Disliked', 'Playing']);
+    //   console.log('patchRecord() response data:', response)
+    // try {
+    // const records = await getFilteredRecords('L');
+    // console.log(records);
+    // console.log(records.length);
+  } catch (error) {
+    // console.error(error);
+    const errorStatus = error.response?.status;
+    const errorStatusText = error.response?.statusText;
+    
+    if (errorStatus && errorStatusText) {
+      console.error(`${errorStatus}: ${errorStatusText}`);
+    }
+  }
+};
+
+
+module.exports = {
+  getFilteredRecords,
+  updateRecordStatus,
+  autorun,
+}
