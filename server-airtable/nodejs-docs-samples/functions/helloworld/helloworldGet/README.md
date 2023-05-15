15514f5342c00cc0ca06d7f9bbeb96a19703cfca
circus2271 <rodionov134@gmail.com>
2023-05-15 06:59:59 +0300

    modify initial code example

--- server-airtable/nodejs-docs-samples/functions/helloworld/helloworldGet/README.md
+++ server-airtable/nodejs-docs-samples/functions/helloworld/helloworldGet/README.md
@@ -5,4 +5,4 @@ https://cloud.google.com/functions/docs/create-deploy-gcloud
 
 repo:
 https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
-
+https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/main/functions/helloworld/helloworldGet
--- server-airtable/nodejs-docs-samples/functions/helloworld/helloworldGet/index.js
+++ server-airtable/nodejs-docs-samples/functions/helloworld/helloworldGet/index.js
@@ -20,6 +20,7 @@ const functions = require('@google-cloud/functions-framework');
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http('helloGET', (req, res) => {
-  res.send('Hello World from sphere!');
-}
+  // res.send('Hello World from sphere!');
+  res.send('Hello World!');
+};
 // [END functions_helloworld_get]
