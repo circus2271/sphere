5d8b1078eccdbd5e65569db6f0bb54f3a00c4bef
circus2271 <rodionov134@gmail.com>
2023-05-19 05:27:23 +0300

    fix a misspelling

--- server-airtable/sphere-google-cloud/getRecords/index.js
+++ server-airtable/sphere-google-cloud/getRecords/index.js
@@ -34,7 +34,7 @@ const getRecords = async () => {
         // how to filter data by multiple keys (in airtable)
         // https://help.landbot.io/article/ngr9wef0b4-how-to-make-the-most-of-advanced-filters-filter-by-formula-airtable-block#3_more_than_one_filter
         
-        // how to check if value contain in a field
+        // how to check if value contains in a field
         // https://help.landbot.io/article/ngr9wef0b4-how-to-make-the-most-of-advanced-filters-filter-by-formula-airtable-block#4_search_filter_contains_value_in_cell_column
         filterByFormula: `AND({Status}='Playing', FIND('Dislike',{Like/Dislike})=0)`
       }
