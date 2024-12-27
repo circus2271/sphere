88a5791ea646f3e2a4d6c34f7fbafd07894f1254
circus2271 <p@WIN-R6PSOGHCMGP.localdomain>
2024-12-27 06:50:34 +0300

    add cloudflare proxy function

---
+++ server-airtable/cloudflare-proxy-worker/src/index.js
@@ -0,0 +1,55 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// also some information about workers is available here:
+  // https://developers.cloudflare.com/workers/get-started/guide/
+
+
+const basePath = 'https://europe-central2-sphere-385104.cloudfunctions.net'
+
+export const loginApiEndpoint = `${basePath}/login`
+export const getRecordsApiEndpoint = `${basePath}/getRecordsFromCdn` // info + tracks
+export const updateRecordApiEndpoint = `${basePath}/updateRecordStatus`
+export const updateSongStatsApiEndpoint = `${basePath}/updateSongStats`
+
+
+export default {
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url)
+		const pathname = url.pathname
+		const searchParams = url.search
+
+		let endpoint;
+
+		switch (pathname) {
+			case('/login'):
+				endpoint = loginApiEndpoint
+				break
+			case('/getRecordsFromCdn'):
+				endpoint = getRecordsApiEndpoint
+				break
+			case('/updateRecordStatus'):
+				endpoint = updateRecordApiEndpoint
+				break
+			case('/updateSongStatsApiEndpoint'):
+				endpoint = updateSongStatsApiEndpoint
+				break
+		}
+
+		if (!endpoint) {
+			return new Response(`api route didn't match, try eiter '/login', '/getRecordsFromCdn', '/updateRecordStatus' or '/updateSongStatsApiEndpoint'`)
+		}
+
+		endpoint += searchParams
+
+		// proxy this request with different url and same options
+		return await fetch(endpoint, request)
+	},
+};
